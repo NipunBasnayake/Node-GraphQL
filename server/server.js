7d1eb896a484441af5887d014bc83cfad4b91b6e
Nipun Basnayake <nipunsathsara1999@gmail.com>
2025-10-24 09:52:06 +0530

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    greet: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    greet: () => 'Hello, world!'
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
